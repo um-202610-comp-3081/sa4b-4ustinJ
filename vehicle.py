862ad1ef0ac9ff17d760dbcfce37512e72844bd6
Austin Jones <swxsh.aj@gmail.com>
2026-02-15 10:47:21 -0600

    Add availability tracking

--- vehicle.py
+++ vehicle.py
@@ -1,14 +1,15 @@
 class Vehicle:
-    def __init__(self, make, model):
+    def __init__(self, make, model, availability):
         self.make = make
         self.model = model
+        self.availability = availability
 
     def __str__(self):
-        return f"{self.make} {self.model}"
+        return f"{self.make} {self.model} | Status: {self.availability}"
 
 
 if __name__ == "__main__":
-    v1 = Vehicle("Toyota", "Camry")
-    v2 = Vehicle("Ford", "F-150")
+    v1 = Vehicle("Toyota", "Camry", "Available")
+    v2 = Vehicle("Ford", "F-150", "In Maintenance")
     print(v1)
     print(v2)
